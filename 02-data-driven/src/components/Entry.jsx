d2f7a2cefb804ba3c2cc41548498757ed9652aa6
Mohamed Wasim Mohamed Akram <63498839+MohamedWasimMohamedAkram@users.noreply.github.com>
2025-04-21 19:56:58 +0300

    Finish Data Driven React along with Travel Journal

--- 02-data-driven/src/components/Entry.jsx
+++ 02-data-driven/src/components/Entry.jsx
@@ -2,18 +2,22 @@ import Marker from "../assets/marker.png";
 function Entry(props) {
   return (
     <article className="articleDiv">
-      <img src={props.img.src} alt={props.img.alt} className="locationImg" />
+      <img
+        src={props.entry.img.src}
+        alt={props.entry.img.alt}
+        className="locationImg"
+      />
       <div className="textSection">
         <div className="countryRow">
           <img src={Marker} alt="Location Marker" className="locationMarker" />
-          <span className="cityName">{props.country}</span>
+          <span className="cityName">{props.entry.country}</span>
           <span className="mapsLink">
-            <a href={props.googleMapsLink}>View on Google Maps</a>
+            <a href={props.entry.googleMapsLink}>View on Google Maps</a>
           </span>
         </div>
-        <h1>{props.title}</h1>
-        <h6>{props.dates}</h6>
-        <p>{props.text}</p>
+        <h1>{props.entry.title}</h1>
+        <h6>{props.entry.dates}</h6>
+        <p>{props.entry.text}</p>
       </div>
     </article>
   );
